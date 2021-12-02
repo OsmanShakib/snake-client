40180f955510d93689ab3ae0d40ab4f386c00019
Osman Shakib <osmanshakib@gmail.com>
2021-12-02 05:58:41 +0000

    changes

--- client.js
+++ client.js
@@ -1,30 +1,31 @@
 const net = require("net");
+const {IP, PORT} = require("./constants");
 
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: OSM")
-     conn.write("Move: up")
-         setTimeout(() => {
-             conn.write("Move: up"); 
-         }, 300)
+    conn.write("Name: OSM");
+    conn.write("Move: up");
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 300);
 
     
-})
+  });
 
-conn.on("data", (data) => {
-    console.log(data)
-})
+  conn.on("data", (data) => {
+    console.log(data);
+  });
   return conn;
 };
 
 console.log("Connecting ...");
-connect();
-module.exports = { connect }
+// connect();
+module.exports = { connect };
